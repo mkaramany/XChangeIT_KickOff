5f2749474f9f062fe8c6dfc03fbe5cd28bb39041
Marwa Elkaramany <marwa.m.ashraf@gmail.com>
2021-01-30 11:03:33 +0300

    Switch SMS implementation from twilio to AWS SNS

--- projectXbackend/src/main/java/com/projectx/xchangeit/service/XChangeItSMSService.java
+++ projectXbackend/src/main/java/com/projectx/xchangeit/service/XChangeItSMSService.java
@@ -2,8 +2,8 @@ package com.projectx.xchangeit.service;
 
 public interface XChangeItSMSService {
 
-	void sendSMS(String to, String body);
-
 	void sendVerificationSMS(String to, String recipientName, String verificationCode);
+
+	void sendSMS(String to, String message, String senderID);
 
 }
