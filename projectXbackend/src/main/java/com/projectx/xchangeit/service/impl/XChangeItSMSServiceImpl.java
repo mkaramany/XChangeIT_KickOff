51c7dcb82b434ec8f56a91c13c08bf719aa90d32
Marwa Elkaramany <marwa.m.ashraf@gmail.com>
2021-01-30 11:05:01 +0300

    SMS with AWS SNS

--- projectXbackend/src/main/java/com/projectx/xchangeit/service/impl/XChangeItSMSServiceImpl.java
+++ projectXbackend/src/main/java/com/projectx/xchangeit/service/impl/XChangeItSMSServiceImpl.java
@@ -1,50 +1,46 @@
 package com.projectx.xchangeit.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.MessageAttributeValue;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
 import com.projectx.xchangeit.service.XChangeItSMSService;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
- 
+
 @Service
-public class XChangeItSMSServiceImpl implements XChangeItSMSService{
-	
-	@Value("${xchangeit.twilio.account.sid}")
-	private String ACCOUNT_SID;
-	
-	@Value("${xchangeit.twilio.account.token}")
-	private String AUTH_TOKEN;
-	
-	@Value("${xchangeit.twilio.number}")
-	private String TWILIO_NUMBER;
+public class XChangeItSMSServiceImpl implements XChangeItSMSService {
 
+	@Value("${xchangeit.sms.senderID}")
+	private String SENDER_ID;
 
-	
 	@Override
-	public void sendSMS(String to, String body){
-		  Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-	        Message message = Message.creator(
-	                new PhoneNumber(to),
-	                new PhoneNumber(TWILIO_NUMBER),
-	                body)
-	                .create();
+	public void sendSMS(String to, String message, String senderID) {
+		AmazonSNSClient snsClient = new AmazonSNSClient();
+		Map<String, MessageAttributeValue> smsAttributes = new HashMap<String, MessageAttributeValue>();
+		smsAttributes.put("AWS.SNS.SMS.SenderID",
+				new MessageAttributeValue().withStringValue(senderID).withDataType("String"));
+		smsAttributes.put("AWS.SNS.SMS.SMSType",
+				new MessageAttributeValue().withStringValue("Promotional").withDataType("String"));
+
+		PublishResult result = snsClient.publish(
+				new PublishRequest().withMessage(message).withPhoneNumber(to).withMessageAttributes(smsAttributes));
+		System.out.println(result);
 	}
-
-
 
 	@Override
 	public void sendVerificationSMS(String to, String recipientName, String verificationCode) {
-		String messageBody = "Dear "+recipientName+", \n"+"Your Verification Code is "+verificationCode;
-		
-		sendSMS(to, messageBody);
-		
-	}
-	
-	
-	
+		String messageBody = "Dear " + recipientName + ", \n" + "Your Verification Code is " + verificationCode;
 
-	
-	
+		sendSMS(to, messageBody, SENDER_ID);
+
+	}
+
 }
