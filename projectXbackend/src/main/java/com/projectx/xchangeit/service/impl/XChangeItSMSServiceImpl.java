f75105f4dfc030d99404b8a4a86aeeca96124262
Marwa Elkaramany <marwa.m.ashraf@gmail.com>
2021-01-31 15:16:16 +0300

    Update AWS SNS Client Construction

--- projectXbackend/src/main/java/com/projectx/xchangeit/service/impl/XChangeItSMSServiceImpl.java
+++ projectXbackend/src/main/java/com/projectx/xchangeit/service/impl/XChangeItSMSServiceImpl.java
@@ -6,24 +6,25 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
 import com.amazonaws.services.sns.model.MessageAttributeValue;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sns.model.PublishResult;
 import com.projectx.xchangeit.service.XChangeItSMSService;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 
 @Service
 public class XChangeItSMSServiceImpl implements XChangeItSMSService {
 
-	@Value("${xchangeit.sms.senderID}")
+	@Value("${xchangeit.aws.sms.senderID}")
 	private String SENDER_ID;
+	
+	@Value("${xchangeit.aws.sms.region}")
+	private String REGION;
 
 	@Override
 	public void sendSMS(String to, String message, String senderID) {
-		AmazonSNSClient snsClient = new AmazonSNSClient();
+		AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(REGION).build();
 		Map<String, MessageAttributeValue> smsAttributes = new HashMap<String, MessageAttributeValue>();
 		smsAttributes.put("AWS.SNS.SMS.SenderID",
 				new MessageAttributeValue().withStringValue(senderID).withDataType("String"));
